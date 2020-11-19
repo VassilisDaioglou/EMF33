509e4202e1f5aa7669f486d1ca2c5c7229093174
VassilisDaioglou <42271684+VassilisDaioglou@users.noreply.github.com>
2020-11-19 17:14:53 +0100

    Major revision: (i) Cleaned up code to make nomenclature more consistent, (ii) Fixed factor levels so that combined plots (EMF33 + Lit) can work, (iii) Present results as boxplots for EMF-33 and Partial Model emission factors

--- script/EmissionSupply.R
+++ script/EmissionSupply.R
@@ -45,7 +45,8 @@ FSizeAxis = 12
 FSizeLeg = 9
 
 ActYears = c(2010,2020,2030,2040,2050)
-
+ActPotBins = factor(c(0,25,50,75,100,125,150,175,200))
+AllPotBins = c(0,25,50,75,100,125,150,175,200,225,250,275,300,325)
 # ---- READ DATA ----
 DATA=read.csv("GitHub/EMF33/data/EmissionSupply/EmissSupplyDat.csv", sep=",", dec=".", stringsAsFactors = FALSE)
 
@@ -65,9 +66,8 @@ clean.data <- function(dataframe){
   data = data %>% mutate(Bio200 = R5B200 - R5B0)
   data = data %>% mutate(Bio300 = R5B300 - R5B0)
   data = data %>% mutate(Bio400 = R5B400 - R5B0)
-  data = data %>% mutate(Bio300LP = R5B300LP - R5B0LP)
 
-  data = subset(data, select = -c(R5B0, R5B100, R5B200, R5B300, R5B400, R5B0LP, R5B300LP))
+  data = subset(data, select = -c(R5B0, R5B100, R5B200, R5B300, R5B400))
   
   data = melt(data, id.vars=c("MODEL","VARIABLE","UNIT","Year","REGION"), na.rm = TRUE)
   colnames(data)[6] <- "SCENARIO"
@@ -106,15 +106,15 @@ rm(Prim.temp)
 # ---- *** Emission Factor Dataframe *** ----
 # Combine data
 EmisFac = Emis.cum
-EmisFac$CumulativeEC = Prim.cum[match(EmisFac$ID,Prim.cum$ID),"Cumulative"]
-EmisFac$PrimEC_2050 = Prim.2050[match(EmisFac$ID,Prim.2050$ID),"value"]
+EmisFac$CumPrimBio = Prim.cum[match(EmisFac$ID,Prim.cum$ID),"Cumulative"]
+EmisFac$PrimBio_2050 = Prim.2050[match(EmisFac$ID,Prim.2050$ID),"value"]
 
 rm(Prim.2050, Prim.cum, Emis.cum)
 # EF calculation
-EmisFac = EmisFac %>% mutate(EF_PrimEC = Cumulative / CumulativeEC)
+EmisFac = EmisFac %>% mutate(EF_PrimBio = Cumulative / CumPrimBio)
 EmisFac$UNIT <- "kgCO2/GJ"
 
-EmisFac = subset(EmisFac, select=-c(VARIABLE, Cumulative, ID, PrimBio, PrimEC))
+EmisFac = subset(EmisFac, select=-c(VARIABLE, Cumulative, CumPrimBio, ID))
 
 # Ascending order 
 EmisFac$EFOrder = factor(EmisFac$SCENARIO, levels = c("Bio100","Bio200","Bio300","Bio300LP","Bio400"))
@@ -122,20 +122,20 @@ EmisFac$EFOrder = gsub( "Bio", "", EmisFac$EFOrder, fixed = F)
 
 # Categorise 2050 production
 bin_width = 50
-EmisFac$PrimEC_2050_bin <- round(EmisFac$PrimEC_2050/(bin_width/10),-1)*(bin_width/10)
-EmisFac$PrimEC_2050_bin = factor(EmisFac$PrimEC_2050_bin, levels = c("0", "50", "100", "150", "200"))
+EmisFac$Pot_bin <- round(EmisFac$PrimBio_2050/(bin_width/10),-1)*(bin_width/10)
+EmisFac$Pot_bin = factor(EmisFac$Pot_bin, levels = c("0", "50", "100", "150", "200"))
 
 # clean
-EmisFac <- EmisFac[!is.infinite(EmisFac$EF_PrimEC),]
+EmisFac <- EmisFac[!is.infinite(EmisFac$EF_PrimBio),]
 
 # ---- *** Emission Factor min/max *** ----
-EMF33_range <- aggregate(EmisFac$EF_PrimEC, by=list(PrimEC_2050_bin=EmisFac$PrimEC_2050_bin, REGION=EmisFac$REGION), FUN=min, na.rm=TRUE) 
+EMF33_range <- aggregate(EmisFac$EF_PrimBio, by=list(Pot_bin=EmisFac$Pot_bin, REGION=EmisFac$REGION), FUN=min, na.rm=TRUE) 
 colnames(EMF33_range)[1] <- "Pot_bin"
 colnames(EMF33_range)[3]<- "Min"
 EMF33_range$ID = paste(EMF33_range$Pot_bin, EMF33_range$REGION)
 
-Max <- aggregate(EmisFac$EF_PrimEC, by=list(PrimEC_2050_bin=EmisFac$PrimEC_2050_bin, REGION=EmisFac$REGION), FUN=max, na.rm=TRUE) 
-Max$ID = paste(Max$PrimEC_2050_bin, Max$REGION)
+Max <- aggregate(EmisFac$EF_PrimBio, by=list(Pot_bin=EmisFac$Pot_bin, REGION=EmisFac$REGION), FUN=max, na.rm=TRUE) 
+Max$ID = paste(Max$Pot_bin, Max$REGION)
 
 EMF33_range$Max = Max[match(EMF33_range$ID,Max$ID),"x"]
 EMF33_range$label <- "EMF-33"
@@ -145,48 +145,63 @@ EMF33_range$ID <- NULL
 rm(Max)
 # 
 # ---- LITERATURE DATA ----
+clean.lit <- function(dataframe,bin,reference){
+  dataframe$CumPrimBio = as.numeric(dataframe$CumPrimBio)
+  dataframe$CumPrimBio = round(dataframe$CumPrimBio,1)
+  dataframe$Pot_bin <- round(dataframe$CumPrimBio/(bin/10),-1)*(bin/10)
+  dataframe$Ref <- reference
+  dataframe
+}
+
 daioglou=read.xlsx("GitHub/EMF33/data/EmissionSupply/Lit_EF.xlsx", sheetName="Daioglou_2017", startRow=1)
 kalt = read.xlsx("GitHub/EMF33/data/EmissionSupply/Lit_EF.xlsx", sheetName="Kalt_2020", startRow=1)
-bin_width2 = 50
+bin_width2 = 25
 
-daioglou <- na.omit(daioglou)
-daioglou$Pot_ExclAg_30yr = round(daioglou$Pot_ExclAg_30yr,0)
-daioglou$Pot_bin <- round(daioglou$Pot_ExclAg_30yr/(bin_width2/10),-1)*(bin_width2/10)
-daioglou$Ref <- "Daiglou_2017"
-daioglou_clean = subset(daioglou, select = c(EF_kgCO2pGJprim, Pot_bin, Ref))
-
-kalt$Pot_bin <- round(kalt$Cum_Pot/(bin_width2/10),-1)*(bin_width2/10)
-kalt$Ref <- "Kalt_2020"
-kalt_clean = subset(kalt, select = c(EF_kgCO2pGJprim, Pot_bin, Ref))
-
-  # Kalt Misses a value for 100 EJ bin
-  missing <- c((max(kalt_clean$EF_kgCO2pGJprim[kalt_clean$Pot_bin==50]) + min(kalt_clean$EF_kgCO2pGJprim[kalt_clean$Pot_bin==150]))/2,
-                            100,
-                            "Kalt_2020")
-  kalt_clean[nrow(kalt_clean)+1,] <- missing[]
+daioglou = clean.lit(daioglou,bin_width2,"Daioglou_2017")
+kalt = clean.lit(kalt,bin_width2,"Kalt_2020")
 
 # Combined dataset and determine ribbon min/max
-Lit = rbind(daioglou_clean,kalt_clean)
+Lit = rbind(daioglou,kalt)
 Lit$EF_kgCO2pGJprim <- as.numeric(Lit$EF_kgCO2pGJprim)
+Lit$Pot_bin = factor(Lit$Pot_bin, levels = AllPotBins)
+
 Lit_range <- aggregate(Lit$EF_kgCO2pGJprim, by=list(Pot_bin=Lit$Pot_bin), FUN=min, na.rm=TRUE) 
 colnames(Lit_range)[2]<- "Min"
 Max <- aggregate(Lit$EF_kgCO2pGJprim, by=list(Pot_bin=Lit$Pot_bin), FUN=max, na.rm=TRUE) 
 Lit_range$Max = Max[match(Lit_range$Pot_bin,Max$Pot_bin),"x"]
 Lit_range$label <- "Literature"
 Lit_range$REGION <- "WORLD"
-rm(daioglou_clean, kalt_clean, Max)
+Lit_range$Pot_bin = factor(Lit_range$Pot_bin, levels = AllPotBins)
+
+rm(daioglou,kalt, Max)
 #
+# ---- COMBINED EMF-33 & LITERATURE ----
+EMF33.temp = subset(EmisFac, REGION == "WORLD")
+EMF33.temp = subset(EMF33.temp, select = c(SCENARIO, Pot_bin, EF_PrimBio))
+EMF33.temp$label <- "EMF-33"
+EMF33.temp$Ref <- "Rose_2021"
+colnames(EMF33.temp)[] <- c("Scenario","Pot_bin","EF_kgCO2pGJprim","label","Ref")
+
+Lit.temp = subset(Lit, select = c(EF_kgCO2pGJprim, Scenario, Pot_bin, Ref))
+Lit.temp$label <- "Partial Models"
+
+EmisFac.all = rbind(EMF33.temp,Lit.temp)
+EmisFac.all$Pot_bin = factor(EmisFac.all$Pot_bin, levels = AllPotBins)
+EmisFac.all = subset(EmisFac.all, Pot_bin %in% ActPotBins)
+
+rm(EMF33.temp, Lit.temp)
 # ---- MIN-MAX EFs (Lit + EMF-33) ----
 EF_ranges = rbind(EMF33_range,Lit_range)
-EF_ranges$Pot_Order = factor(EF_ranges$Pot_bin, levels =c("0","50","100","150","200"))
+EF_ranges$Pot_bin = factor(EF_ranges$Pot_bin, levels = AllPotBins) #c("0","25","50","75","100","125","150","175","200","225","250","275","300","325"))
+EF_ranges = subset(EF_ranges, Pot_bin %in% ActPotBins)
 #
 # ---- FIGURES ----
 # ---- *** FIG: Literature data - lineplot  ----
 Figlit<-ggplot() + 
-  geom_line(data = daioglou, aes(x = Pot_ExclAg_30yr, y = EF_kgCO2pGJprim), colour = "red", size = 1) +
-  geom_line(data = kalt, aes(x = Cum_Pot, y = EF_kgCO2pGJprim), colour = "blue", size = 1) +
+  geom_point(data = Lit, aes(x = CumPrimBio, y = EF_kgCO2pGJprim, colour = Scenario), size = 1) +
   geom_hline(yintercept=0,size = 0.1, colour='black') +
   geom_vline(xintercept=0,size = 0.1, colour='black') +
+  ylim(0,100) +
   theme_bw() +
   theme(text= element_text(size=6, face="plain"), axis.text.x = element_text(angle=90, size=6, hjust=0.5), axis.text.y = element_text(size=6)) +
   theme(panel.border = element_rect(colour = "black", fill=NA, size=0.2)) +
@@ -195,8 +210,8 @@ Figlit<-ggplot() +
 Figlit
 #
 # ---- *** FIG: EMF-33 EF-Curve - boxplot  ----
-boxplot<-ggplot(data = subset(EmisFac, REGION == "WORLD" & !SCENARIO=="Bio300LP"),
-                 aes(x=PrimEC_2050_bin, y = EF_PrimEC)) + 
+boxplot<-ggplot(data = subset(EmisFac, REGION == "WORLD"),
+                 aes(x=Pot_bin, y = EF_PrimBio)) + 
   geom_boxplot() +
   geom_jitter(aes(colour = MODEL, shape=SCENARIO),
               width=0.1, alpha = 0.9) +
@@ -210,8 +225,8 @@ boxplot<-ggplot(data = subset(EmisFac, REGION == "WORLD" & !SCENARIO=="Bio300LP"
 boxplot
 #
 # ---- *** FIG: EMF-33 EF-Curve - lineplot  ----
-line<-ggplot(data = subset(EmisFac, REGION == "WORLD" & !SCENARIO=="Bio300LP"),
-                aes(x=PrimEC_2050, y = EF_PrimEC,colour = MODEL)) + 
+line<-ggplot(data = subset(EmisFac, REGION == "WORLD"),
+                aes(x=PrimBio_2050, y = EF_PrimBio,colour = MODEL)) + 
   geom_line() +
   geom_point(aes(shape=SCENARIO)) +
   geom_hline(yintercept=0,size = 0.1, colour='black') +
@@ -223,15 +238,13 @@ line<-ggplot(data = subset(EmisFac, REGION == "WORLD" & !SCENARIO=="Bio300LP"),
   xlab("EJ Primary Biomass") 
 line
 #
-# ---- *** FIG: EMF-33 EF (box) + Literature (ribbon)  ----
-combined<-ggplot() + 
-  geom_boxplot(data = subset(EmisFac, REGION == "WORLD" & !SCENARIO=="Bio300LP"),
-               aes(x=PrimEC_2050_bin, y = EF_PrimEC)) +
-  # geom_jitter(aes(colour=MODEL),
-  #             width=0.2, alpha = 0.9) +
-  geom_ribbon(data = subset(EF_ranges, label == "Literature"),
-              inherit.aes = FALSE,
-              aes(x = Pot_Order, ymin=Min, ymax=Max, group = label, fill=label), alpha = "0.25") +
+# ---- *** FIG: EMF-33 EF + Literature (boxplot)  ----
+boxed<-ggplot() + 
+  geom_boxplot(data = EmisFac.all,
+               aes(x=Pot_bin, y = EF_kgCO2pGJprim, fill = label),
+               size = 0.5, outlier.shape = NA) +
+  ylim(0,100) +
+  scale_x_discrete(limits=ActPotBins) +
   geom_hline(yintercept=0,size = 0.1, colour='black') +
   geom_vline(xintercept=0,size = 0.1, colour='black') +
   ylab(expression(paste("kgCO"[2],"/GJ-Prim"))) + 
@@ -239,13 +252,12 @@ combined<-ggplot() +
   theme_bw() +
   theme(text= element_text(size=6, face="plain"), axis.text.x = element_text(angle=90, size=6, hjust=0.5), axis.text.y = element_text(size=6)) +
   theme(panel.border = element_rect(colour = "black", fill=NA, size=0.2)) +
-  theme(legend.position="bottom", legend.box = "vertical", legend.title= element_text(face="bold.italic")) +
-  scale_color_discrete(name = "EMF-33 IAM:") +
-  scale_fill_manual(values="dodgerblue",
-                  name="Partial Models:",
-                  breaks="Literature",
-                  labels="Kalt et al. (2020) & Daioglou et al. (2017)")
-combined
+  theme(legend.position="right", legend.box = "vertical", legend.title= element_text(face="bold.italic"))  +
+  scale_fill_manual(values=c("darksalmon","dodgerblue"),
+                  name="Model type",
+                  breaks=c("Partial Models","EMF-33"),
+                  labels=c("Partial Models","EMF-33 (IAM)"))
+boxed
 #
 # ---- *** FIG: EMF-33 EF Literature (double ribbon)  ----
 ribboned<-ggplot() + 
@@ -275,14 +287,14 @@ ribboned
 # plot(line)
 # dev.off()
 # # #
-# png(file = "GitHub/EMF33/output/EmissionSupply/Combined.png", width = 4*ppi, height = 4*ppi, units = "px", res = ppi)
-# plot(combined)
+# png(file = "GitHub/EMF33/output/EmissionSupply/Boxed.png", width = 4*ppi, height = 2*ppi, units = "px", res = ppi)
+# plot(boxed)
 # dev.off()
 # # #
 # png(file = "GitHub/EMF33/output/EmissionSupply/Ribboned.png", width = 4*ppi, height = 2*ppi, units = "px", res = ppi)
 # plot(ribboned)
 # dev.off()
 # # #
-# png(file = "GitHub/EMF33/output/EmissionSupply/LineLit.png", width = 4*ppi, height = 4*ppi, units = "px", res = ppi)
+# png(file = "GitHub/EMF33/output/EmissionSupply/Line_Lit.png", width = 4*ppi, height = 4*ppi, units = "px", res = ppi)
 # plot(Figlit)
 # dev.off()
