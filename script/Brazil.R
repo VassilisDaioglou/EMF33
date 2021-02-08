d2bc8f712a944da1f48538eea5c5a88db8108c2b
VassilisDaioglou <42271684+VassilisDaioglou@users.noreply.github.com>
2021-02-08 17:28:24 +0100

    Added new figure which compares brazilian and global emission strategies

--- script/Brazil.R
+++ script/Brazil.R
@@ -66,8 +66,34 @@ TotEmis <- ggplot() +
   facet_wrap(~MODEL, scales="free_y", labeller=labeller(MODEL = model_labels))
 TotEmis
 
+GlobEmis <- ggplot() + 
+  geom_line(data=subset(BraDATA,  SCENARIO == "R3-B-lo-full" & VARIABLE == "Emissions|CO2"),
+            aes(x=Year,y = value, color=VARIABLE), size=1, alpha=1) +
+  geom_line(data=subset(BraDATA, SCENARIO == "R3-B-lo-full"  & !VARIABLE == "Emissions|CO2"),
+            aes(x=Year,y = value, color=VARIABLE), size=1, alpha=0.75) +
+  geom_hline(yintercept=0,size = 0.1, colour='black') +
+  ylab("Emissions MtCO2/yr") + xlab("") +
+  theme_bw() +  theme(panel.grid.minor=element_blank(), panel.grid.major=element_line(colour="gray80", size = 0.3)) +
+  theme(plot.title = element_text(size = FSizeTitle, face = "bold")) +
+  theme(text= element_text(size=FSizeStrip, face="plain"), axis.text.x = element_text(angle=66, size=FSizeAxis, hjust=1), axis.text.y = element_text(size=FSizeAxis)) +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=0.2)) +
+  theme(legend.position="bottom", legend.box="vertical", legend.direction = "horizontal", legend.spacing.y=unit(0.01,"cm")) +
+  scale_colour_manual(values=c("black","red","forestgreen"),
+                      name="Emission Source:",
+                      breaks=c("Emissions|CO2","Emissions|CO2|Energy","Emissions|CO2|Land Use"),
+                      labels=c("Total","Energy","AFOLU"),
+                      guide="legend") +
+  theme(strip.text.x = element_text(size = FSizeStrip, face="bold"), strip.text.y = element_text(size = FSizeStrip, face="bold")) +
+  facet_grid(REGION~MODEL, scales="free_y", labeller=labeller(MODEL = model_labels))
+GlobEmis
+
+
 # ---- OUTPUT ----
 # png(paste0(getwd(),"/GitHub/EMF33/output/Brazil/Emissions.png"), width=6*ppi, height=5*ppi, res=ppi)
 # print(plot(TotEmis))
+# dev.off()
+# 
+# png(paste0(getwd(),"/GitHub/EMF33/output/Brazil/GlobalEmissions.png"), width=10*ppi, height=5*ppi, res=ppi)
+# print(plot(GlobEmis))
 # dev.off()
 # 
