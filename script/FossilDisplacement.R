cc4bb17df59c79ce7abbb231ed2d66522e1d6491
VassilisDaioglou <42271684+VassilisDaioglou@users.noreply.github.com>
2021-02-17 12:03:18 +0100

    Determined year where each projections surpasses 100$/tCO2 in mitigation scenarios. Determine this per MODEL, SCENARIO, and REGION

--- script/FossilDisplacement.R
+++ script/FossilDisplacement.R
@@ -9,6 +9,10 @@
 # 2. Determine extent of bioenergy use in these energy carriers across a mitigation scenario
 # 3. Determine displaced CO2
 #
+# Calculations are done for a carbon price of approximately 100 $/tCO2, in order to be consistent with BECCS
+# potential calculations. Sinnce the EMF-33 projections do not include results for a carbon price of exactly 100$/tCO2,
+# we instead identify the timestep that car bon prices are projected to surpass 100$/tCO2, and use the data of that timestep.
+#
 # AUTHORSHIP:
 # Author: Vassilis Daioglou
 # Date: February 2020
@@ -30,7 +34,7 @@ library(xlsx)
 library(openxlsx)
 library(ggpubr)
 
-setwd("C:/Users/Asus/Documents/Github/SHAPE/")
+setwd("C:/Users/Asus/Documents/Github/EMF33/")
 
 # ---- INPUTS: Constants ----
 ppi <- 300
@@ -38,3 +42,23 @@ FSizeTitle = 10
 FSizeStrip = 9
 FSizeAxis = 9
 FSizeLeg = 9
+
+# ---- READ DATA ----
+DATA=read.csv("data/FossilDisplacement/DisplacementData.csv", sep=",", dec=".", stringsAsFactors = FALSE)
+
+DATA$X <- NULL
+DATA = subset(DATA, !(Year=="1990"|Year=="1995"|Year=="2000"|Year=="2005"|Year=="2010"|Year=="2015"|Year=="2110"|Year=="2130"|Year=="2150"))
+DATA$ID1 = paste(DATA$MODEL, DATA$SCENARIO, DATA$REGION)
+DATA$ID2 = paste(DATA$MODEL, DATA$SCENARIO, DATA$REGION,DATA$Year)
+
+cprice = subset(DATA, VARIABLE == "Price|Carbon")
+cprice$Tech <- NULL
+# ---- YEAR OF INTEREST ----
+# Identify first year when carbon prices are above 100$/tCO2
+Above100 = subset(cprice, value > 100)
+Cross100 = aggregate(Above100$Year, by=list(ID1=Above100$ID1), FUN=min, na.rm=TRUE)
+Cross100$ID2 = paste(Cross100$ID1, Cross100$x)
+
+DATA.cor = subset(DATA, ID2 %in% Cross100$ID2 & !(VARIABLE=="Price|Carbon"))
+
+# ----  CARBON CONTENTS OF FINAL ENERGY CARRIERS ----
